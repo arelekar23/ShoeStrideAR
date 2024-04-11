3a48c43be9923a7eb5dab27273e453c8f41c6fdd
amarneu <nagargoje.a@northeastern.edu>
2024-04-11 00:38:53 -0400

    added UI - HomeView, CartView

---
+++ StepVision/Models/ShoeCell.swift
@@ -0,0 +1,9 @@
+//
+//  ShoeCell.swift
+//  StepVision
+//
+//  Created by Amar Nagargoje on 4/10/24.
+//  Copyright © 2024 Snap. All rights reserved.
+//
+
+import Foundation
---
+++ StepVision/Views/CollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewCell.swift
+//  StepVision
+//
+//  Created by Amar Nagargoje on 4/10/24.
+//  Copyright © 2024 Snap. All rights reserved.
+//
+
+import Foundation
